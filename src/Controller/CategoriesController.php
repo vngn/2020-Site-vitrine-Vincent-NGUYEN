c2678c9bbad3c0dd88c8a6b63755a0f28265eb14
vngn <vngn@laposte.net>
2020-08-27 11:50:47 +0200

    ajout des namespace isgranted dans controlleurs

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -7,6 +7,7 @@ use App\Form\CategoriesType;
 use App\Repository\CategoriesRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
