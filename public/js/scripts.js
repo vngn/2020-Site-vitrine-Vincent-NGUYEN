445aca2ee2c3b91ec2cb0eaca32a4eb963f9f418
vngn <vngn@laposte.net>
2020-08-31 15:40:44 +0200

    canvas présentation page home en comment

---
+++ public/js/scripts.js
@@ -0,0 +1,23 @@
+// var c = document.getElementById('canvas');
+// var btn = document.getElementsByClassName('btn-canvas')[0];
+
+// var txtPosition = 0;
+
+// btn.addEventListener('mouseup', function(e){
+//     changeText();
+// });
+
+// // Text
+
+// var btnTxt = [
+//     'Vincent NGUYEN',
+//     'Développeur de site Web',
+//     'Bienvenue sur mon sitre vitrine'    
+// ];
+
+// function changeText(){
+//     if(txtPosition !== btnTxt.length){
+//         btn.innerHTML = btnTxt[txtPosition];
+//         txtPosition += 1;
+//     }
+// }
