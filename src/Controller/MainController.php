8a9f644f9565276d62bb2dca7483ee8ab3aed138
vngn <vngn@laposte.net>
2020-09-01 10:06:36 +0200

    affichage des lenght page home pour visitors ok. warning lf to crlf

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\BlogRepository;
+use App\Repository\ArticlesRepository;
+use App\Repository\PortfolioRepository;
+use App\Repository\BlogCommentRepository;
+use App\Repository\ArticlesCommentRepository;
+use App\Repository\PortfolioCommentRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index()
+    public function index(PortfolioRepository $portfolioRepo, PortfolioCommentRepository $portfolioCommentRepo, ArticlesRepository $articlesRepo, ArticlesCommentRepository $articlesCommentRepo, BlogRepository $blogRepo, BlogCommentRepository $blogCommentRepo)
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'MainController'
+            'controller_name' => 'MainController',
+            'portfolio' => $portfolioRepo->findAll(),
+            'portfolioComment' => $portfolioCommentRepo->findAll(),
+            'articles' => $articlesRepo->findAll(),
+            'articlesComment' => $articlesCommentRepo->findAll(),
+            'blog' => $blogRepo->findAll(),
+            'blogComment' => $blogCommentRepo->findAll()
         ]);
     }
 }
