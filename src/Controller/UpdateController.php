8c9b4de7e152907c738def4b81dcc0afacfae24f
vngn <vngn@laposte.net>
2020-09-14 11:55:56 +0200

    Update UpdateController.php

--- src/Controller/UpdateController.php
+++ src/Controller/UpdateController.php
@@ -2,18 +2,98 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Update;
+use App\Form\UpdateType;
+use App\Repository\UsersRepository;
+use App\Repository\UpdateRepository;
+use App\Repository\ContactRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/update")
+ * @package App\Controller
+ */
 
 class UpdateController extends AbstractController
 {
     /**
-     * @Route("/update", name="update")
+     * @Route("/", name="update_index")
      */
-    public function index()
+    public function index(UpdateRepository $catsRepo, ContactRepository $contactRepo, UsersRepository $usersRepo)
     {
         return $this->render('update/index.html.twig', [
-            'controller_name' => 'UpdateController',
+            'update' => $catsRepo->findAll(),
+            'contact' => $contactRepo->findAll(),
+            'users' => $usersRepo->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/add", name="update_add")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function add(Request $request, ContactRepository $contactRepo, UsersRepository $usersRepo)
+    {
+        $update = new Update;
+
+        $form = $this->createForm(UpdateType::class, $update);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($update);
+            $em->flush();
+
+            return $this->redirectToRoute('update_index');
+        }
+
+        return $this->render('update/add.html.twig', [
+            'form' => $form->createView(),
+            'contact' => $contactRepo->findAll(),
+            'users' => $usersRepo->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="update_edit")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function edit(Update $update, Request $request, ContactRepository $contactRepo, UsersRepository $usersRepo)
+    {
+        $form = $this->createForm(UpdateType::class, $update);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($update);
+            $em->flush();
+
+            return $this->redirectToRoute('update_index');
+        }
+
+        return $this->render('update/add.html.twig', [
+            'form' => $form->createView(),
+            'contact' => $contactRepo->findAll(),
+            'users' => $usersRepo->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="update_delete")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function delete(Update $update)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($update);
+        $em->flush();
+
+        $this->addFlash('message', 'update supprimée avec succès');
+        return $this->redirectToRoute('update_index');
     }
 }
