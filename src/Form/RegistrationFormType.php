0be5e054e554c56a98d19d6665931f38be4a6ab8
vngn <vngn@laposte.net>
2020-09-20 10:43:18 +0200

    ajout de sécurité par répétition du mdp à l'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,6 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -23,21 +24,21 @@ class RegistrationFormType extends AbstractType
                 'attr' => [
                     'class' => 'name-form',
                     'placeholder' => 'votre nom...'
-                    ]
+                ]
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom :',
                 'attr' => [
                     'class' => 'firstname-form',
-                    'placeholder' => 'votre prénom...'                    
-                    ]
+                    'placeholder' => 'votre prénom...'
+                ]
             ])
             ->add('email', TextType::class, [
                 'label' => 'Email :',
                 'attr' => [
                     'class' => 'email-form',
-                    'placeholder' => 'votre Email...'                    
-                    ]
+                    'placeholder' => 'votre Email...'
+                ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'label' => 'J’accepte les conditions mentionnées ci-dessus et la politique de confidentialité : ',
@@ -48,25 +49,37 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer un mot de passe'
+                        ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => 'Mot de passe : ',
+                    'attr' => [
+                        'class' => 'password-form',
+                        'placeholder' => 'votre mot de passe...'
+                        ]
+                ],
+                'second_options' => [
+                    'label' => 'Répéter le mot de passe : ',
+                    'attr' => [
+                        'class' => 'password-form',
+                        'placeholder' => 'répéter votre mot de passe...'
+                        ]
+                ],
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => [
-                    'class' => 'password-form',
-                    'placeholder' => 'votre mot de passe...'  
-                ],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez entrer un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
             ]);
     }
 
